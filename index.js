f24fe67ee5e6494336960ba4c5927a053cb8fa16
ulivz <472590061@qq.com>
2017-09-17 02:19:25 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const Handlebars = require('handlebars')
+const { setupHelpers } = require('./lib')
+
+setupHelpers(Handlebars)
+
+module.exports = Handlebars
+
+module.exports.render = function render(tmp, context) {
+  const compiler = Handlebars.compile(tmp)
+  return compiler(context)
+}
+
+module.exports.renderPartial = (name, context) => {
+  let partial = Handlebars.partials[name]
+  return partial ? partial(context) : (new Error(`cannot find partial ${name}`))
+}
